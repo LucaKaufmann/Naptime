7c9193ff39f14a961e4c0bfa55fe2f601e60d9c2
Luca Kaufmann <luca@tibber.com>
2023-01-09 15:44:22 +0200

    adding tests

--- NapTimeData/Tests/NapTimeDataTests/ActivityPersistenceTests.swift
+++ NapTimeData/Tests/NapTimeDataTests/ActivityPersistenceTests.swift
@@ -6,7 +6,7 @@
 //
 
 import XCTest
-@testable import Naptime
+@testable import NapTimeData
 
 final class ActivityPersistenceTests: XCTestCase {
     
--- Naptime/Activity/ActivityView.swift
+++ Naptime/Activity/ActivityView.swift
@@ -13,46 +13,80 @@ struct ActivityView: View {
     let store: Store<Activity.State, Activity.Action>
     
     var body: some View {
-        WithViewStore(self.store) { viewStore in
-            VStack {
-                Text("Activities")
-                Button(action: {
-                    viewStore.send(.startActivity(.sleep))
-                }, label: {
-                    Text("Add activity")
-                })
-                List {
-                    ForEach(viewStore.activityHeaderDates, id: \.self) { header in
-                        Section(header: ActivitySectionHeaderView(date: header)) {
-                            ForEach(viewStore.groupedActivities[header]!) { activity in
-                                ActivityRowView(activity: activity)
+        GeometryReader { geometry in
+            WithViewStore(self.store) { viewStore in
+                ZStack {
+                    BackgroundShape()
+                        .foregroundColor(.blue)
+                        .frame(width: geometry.size.width, height: geometry.size.height)
+//                        .frame(width: 100, height: 100)
+                    VStack {
+                        Text("Activities")
+                        Button(action: {
+                            viewStore.send(.startActivity(.sleep))
+                        }, label: {
+                            Text("Add activity")
+                        })
+                        ScrollView {
+                            ForEach(viewStore.activityHeaderDates, id: \.self) { header in
+                                Section(header: ActivitySectionHeaderView(date: header)) {
+                                    ForEach(viewStore.groupedActivities[header]!) { activity in
+                                        ActivityRowView(activity: activity)
+                                    }
+                                }
                             }
                         }
                     }
                 }
-//                List(viewStore.activities) { activity in
-//                    HStack {
-//                        Text("\(activity.type.rawValue)")
-//                        VStack {
-//                            Text(activity.startDate, style: .time)
-//                            if let endDate = activity.endDate {
-//                                Text(endDate, style: .time)
-//                            } else {
-//                                Text("...")
-//                            }
-//                        }
-//                    }
-//                }
             }
-        }
+        }.ignoresSafeArea()
+    }
+}
+
+struct BackgroundShape : Shape {
+    func path(in rect: CGRect) -> Path {
+        var p = Path()
+        let topLeftCorner = rect.height / 3
+        let radius = rect.height*1.5/2
+        let startingPoint = CGPoint(
+            x: 0,
+            y: topLeftCorner
+        )
+        p.move(
+           to: startingPoint
+       )
+//        p.addLine(
+//            to: CGPoint(
+//                x: rect.width,
+//                y: topLeftCorner)
+//        )
+        p.addArc(center: CGPoint(x: rect.width/2, y:rect.height), radius: radius, startAngle: .degrees(-125), endAngle: .degrees(-55), clockwise: false)
+        p.addLine(
+            to: CGPoint(
+                x: rect.width,
+                y: rect.height)
+        )
+        p.addLine(
+            to: CGPoint(
+                x: 0,
+                y: rect.height)
+        )
+        p.addLine(
+            to: startingPoint
+        )
+
+        return p
     }
 }
 
 struct ActivityView_Previews: PreviewProvider {
     static var previews: some View {
+        let date = Date()
+        let activities = [ActivityModel(id: UUID(), startDate: date, endDate: nil, type: .sleep)]
+        let grouped: [Date: [ActivityModel]] = [date: activities]
         ActivityView(
             store: Store(
-                initialState: Activity.State(activities: [], groupedActivities: [Date: [ActivityModel]](), activityHeaderDates: []),
+                initialState: Activity.State(activities: activities, groupedActivities: grouped, activityHeaderDates: [Date()]),
                 reducer: Activity()))
     }
 }
